920b245fa21f71760b423971be81e42192d21650
mkrol1997 <golotka333@gmail.com>
2023-10-15 14:18:08 +0200

    Debug False

--- pixel_perfect/pixel_perfect/settings.py
+++ pixel_perfect/pixel_perfect/settings.py
@@ -20,7 +20,7 @@ load_dotenv(find_dotenv())
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST_1')]
 
