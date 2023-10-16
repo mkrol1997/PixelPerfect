c714fd4548c24594b106b02c0ea9c61586657f40
mkrol1997 <golotka333@gmail.com>
2023-10-16 22:02:46 +0200

    Fixed env var

--- pixel_perfect/pixel_perfect/settings.py
+++ pixel_perfect/pixel_perfect/settings.py
@@ -139,11 +139,11 @@ WSGI_APPLICATION = "pixel_perfect.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
