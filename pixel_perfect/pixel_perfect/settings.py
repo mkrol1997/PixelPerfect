d1639e0b16c8394fe84001d56ac3162234e23171
mkrol1997 <golotka333@gmail.com>
2023-10-17 15:05:58 +0200

    Removed test db

--- pixel_perfect/pixel_perfect/settings.py
+++ pixel_perfect/pixel_perfect/settings.py
@@ -145,10 +145,6 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
     },
-    'test': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
 
 # Password validation
