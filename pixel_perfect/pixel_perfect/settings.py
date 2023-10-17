55f34dc0d78166036dd0d23f24391ddb7852f631
mkrol1997 <golotka333@gmail.com>
2023-10-17 17:17:49 +0200

    Added psql contanier

--- pixel_perfect/pixel_perfect/settings.py
+++ pixel_perfect/pixel_perfect/settings.py
@@ -138,7 +138,7 @@ WSGI_APPLICATION = "pixel_perfect.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
